a132b60825d16fb1655b33a0b1040761fb2c7485
Alejandro Tomasella <aletomasella09@gmail.com>
2024-02-11 14:41:13 -0300

    Testing Rust Rover

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{fs::File, io::Write};
+use std::io::Read;
 
 fn main() -> std::io::Result<()> {
     let mut idx = 0;
@@ -9,7 +10,13 @@ fn main() -> std::io::Result<()> {
             break;
         };
 
-        lines.push(format!("LINE NUMBER: {}", idx));
+        // if odd we insert that if not we insert the other
+        if idx % 2 == 0 {
+            lines.push(format!("Hello, Rust! {}", idx));
+        } else {
+            lines.push(format!("We are trying the new Rust IDE: {}", idx));
+        }
+
         idx += 1;
     }
 
@@ -17,7 +24,26 @@ fn main() -> std::io::Result<()> {
 
     file.write_all(lines.join("\n").as_bytes())?;
 
-    println!("DONE!");
+    // After the file is written, we can print a message to the console
+
+
+
+ // Then we need to get what we write
+
+    let mut file = File::open("text.txt")?;
+
+    let mut contents = String::new();
+
+
+    file.read_to_string(&mut contents)?;
+
+    // Print first lines
+
+    let first_lines = contents.lines().take(5);
+
+    for line in first_lines {
+        println!("{}", line);
+    }
 
     Ok(())
 }
