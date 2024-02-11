f6c6da7c2bffdc68dfd9a4621ed7c029752676c0
Alejandro Tomasella <aletomasella09@gmail.com>
2024-02-10 23:20:41 -0300

    feat : Reading bytes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+extern crate tun_tap;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+
+    let mut buf = [0u8; 1504];
+
+    let nbytes = nic.recv(&mut buf[..])?;
+
+    eprint!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+    Ok(())
 }
